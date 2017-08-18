2e371693141bb4139b5090882d8fb6d3b88f3a70
Mariano <mmontori@umich.edu>
2017-08-18 11:01:11 -0400

    a little bit of color changes

--- GiftBag/AppDelegate.swift
+++ GiftBag/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         configureInitialRootViewController(for: window)
+        UINavigationBar.appearance().barTintColor = Styles.lightGreen
 //        for family: String in UIFont.familyNames
 //        {
 //            print("\(family)")
